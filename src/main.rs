39d5592b138dc88ab0be5da8af3f2891b73a2c10
LE0N9RE <leondre0nosworthy@gmail.com>
2026-06-24 19:26:58 -0400

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::net::TcpListener;
-fn main() {
-    //listen for incoming connections
-    let net = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in net.incoming() {
-        // grab the connection.
+
+fn node() {
+    let args: Vec<String> = std::env::args().collect();
+    let chosen_port = &args[1];
+    let addr = format!("127.0.0.1:{}", chosen_port);
+    println!("Starting node on: {}", addr);
+    let riv = TcpListener::bind(addr).unwrap();
+
+    for stream in riv.incoming() {
         let _stream = stream.unwrap();
         println!("Connection established! Someone just pinged our server!");
     }
+}
+fn main() {
+    node();
 }
